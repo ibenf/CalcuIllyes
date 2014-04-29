93440546cc3cddebf4febc899a3f2fec43b4b8b2
Ibenf <illyesbf@yahoo.fr>
2014-04-29 09:57:12 +0200

    Update calculatrice.java

--- calculatrice.java
+++ calculatrice.java
@@ -1,219 +1,96 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.*; 
+import java.awt.event.*; 
+import javax.swing.*; 
 
-  
-public class calculatrice extends JFrame {
-  private JPanel container = new JPanel();
-  
-  String[] tableau = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0","=", "effacer", "+", "-", "*", "/"};
+public class calculatrice implements ActionListener 
+{ 
 
-  JButton[] tableau_bouton = new JButton[16];
-  private JLabel ecran = new JLabel();
-  
-  private double chiffre1; 
-  private boolean clicOperateur = false, update = false;
-  private String operateur = "";
-  
-  public calculatrice(){
-    this.setSize(300,250);
-    this.setTitle("Calculette");
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setLocationRelativeTo(null);
-    initialiserInterface();
-	this.setContentPane(container);
-	this.setVisible(true);
-  }
-      
-  private void initialiserInterface(){
-    
-	  Font police = new Font("Arial", Font.BOLD, 20);
-	    ecran = new JLabel("0");
-	    ecran.setFont(police);
-   
-	  ecran.setHorizontalAlignment(JLabel.RIGHT);
-	    ecran.setPreferredSize(new Dimension(220, 20));
-	    JPanel operateur = new JPanel();      
-	    operateur.setPreferredSize(new Dimension(55, 225));
-	    JPanel chiffre = new JPanel();
-	    chiffre.setPreferredSize(new Dimension(165, 225));
-	    JPanel panEcran = new JPanel();
-	    panEcran.setPreferredSize(new Dimension(220, 30));
+JTextArea affichage; 
 
-   
-    for(int i = 0; i < 16; i++){
-    	tableau_bouton[i] = new JButton(tableau[i]);
-       
-      switch(i){
-        
-        case 10 :
-        	tableau_bouton[i].addActionListener(new Egalite());
-          chiffre.add(tableau_bouton[i]);
-          break;
-        case 11 :
-        	tableau_bouton[i].setForeground(Color.red);
-        	tableau_bouton[i].addActionListener(new Annulation());
-          operateur.add(tableau_bouton[i]);
-          break;
-        case 12 :
-        	tableau_bouton[i].addActionListener(new Addition());
-          
-          operateur.add(tableau_bouton[i]);
-          break;
-        case 13 :
-        	tableau_bouton[i].addActionListener(new Soustraction());
-          
-          operateur.add(tableau_bouton[i]);
-          break;	
-        case 14 :	
-        	tableau_bouton[i].addActionListener(new Multiplication());
-          
-          operateur.add(tableau_bouton[i]);
-          break;
-        case 15 :
-        	tableau_bouton[i].addActionListener(new Division());
-          
-          operateur.add(tableau_bouton[i]);
-          break;
-        default :
-          
-          chiffre.add(tableau_bouton[i]);
-          tableau_bouton[i].addActionListener(new AfficherChiffre());
-          break;
-      }
-    }
-    panEcran.add(ecran);
-    panEcran.setBorder(BorderFactory.createLineBorder(Color.black));
-    panEcran.setSize(10,10);
-    container.add(panEcran, BorderLayout.CENTER);
-    container.add(chiffre, BorderLayout.NORTH);
-    container.add(operateur, BorderLayout.EAST);
-  }
+public calculatrice() 
+{ 
 
-  
-  private void calcul(){
-    if(operateur.equals("+")){
-      chiffre1 = chiffre1 + 
-            Double.valueOf(ecran.getText()).doubleValue();
-      ecran.setText(String.valueOf(chiffre1));
-    }
-    if(operateur.equals("-")){
-      chiffre1 = chiffre1 - 
-            Double.valueOf(ecran.getText()).doubleValue();
-      ecran.setText(String.valueOf(chiffre1));
-    }          
-    if(operateur.equals("*")){
-      chiffre1 = chiffre1 * 
-            Double.valueOf(ecran.getText()).doubleValue();
-      ecran.setText(String.valueOf(chiffre1));
-    }     
-    if(operateur.equals("/")){
-      try{
-        chiffre1 = chiffre1 / 
-              Double.valueOf(ecran.getText()).doubleValue();
-        ecran.setText(String.valueOf(chiffre1));
-      } catch(ArithmeticException e) {
-        ecran.setText("0");
-      }
-    }
-  }
+JFrame all = new JFrame("calculatrice" ); 
 
+JPanel back = new JPanel(); 
+JPanel clavier = new JPanel(); 
+JPanel operation = new JPanel(); 
+JPanel clavscien= new JPanel(); 
+JPanel egal=new JPanel(); 
 
-  
-  class AfficherChiffre implements ActionListener {
-    public void actionPerformed(ActionEvent e){
-     
-      String str = ((JButton)e.getSource()).getText();
-      if(update){
-        update = false;
-      }
-      else{
-        if(!ecran.getText().equals("0"))
-          str = ecran.getText() + str;
-      }
-      ecran.setText(str);
-    }
-  }
+egal.setLayout(new BorderLayout()); 
+back.setLayout(new BorderLayout()); 
+operation.setLayout(new GridLayout(4,1)); 
+clavscien.setLayout(new GridLayout(2,1)); 
+clavier.setLayout(new GridLayout(4,3)); 
 
+JButton t1 = new JButton("1" ); 
+t1.addActionListener(this); 
+JButton t2 = new JButton("2" ); 
+t2.addActionListener(this); 
+JButton t3 = new JButton("3" ); 
+t3.addActionListener(this); 
+JButton t4 = new JButton("4" ); 
+t4.addActionListener(this); 
+JButton t5 = new JButton("5" ); 
+t5.addActionListener(this); 
+JButton t6 = new JButton("6" ); 
+t6.addActionListener(this); 
+JButton t7 = new JButton("7" ); 
+t7.addActionListener(this); 
+JButton t8 = new JButton("8" ); 
+t8.addActionListener(this); 
+JButton t9 = new JButton("9" ); 
+t9.addActionListener(this); 
+JButton t10 = new JButton("0" ); 
+t10.addActionListener(this); 
+JButton t11 = new JButton("," ); 
+t11.addActionListener(this); 
+JButton t12 = new JButton("-" ); 
+t12.addActionListener(this); 
+JButton plus = new JButton("+" ); 
+plus.addActionListener(this); 
+JButton moins = new JButton("-" ); 
+moins.addActionListener(this); 
+JButton fois = new JButton("*" ); 
+fois.addActionListener(this); 
+JButton divise = new JButton("/" ); 
+divise.addActionListener(this); 
+JButton carre = new JButton("²" ); 
+carre.addActionListener(this); 
+JButton cube = new JButton("³" ); 
+cube.addActionListener(this); 
+JButton tegal = new JButton("=" ); 
+tegal.addActionListener(this); 
 
-  class Egalite implements ActionListener {
-    public void actionPerformed(ActionEvent arg0){
-      calcul();
-      update = true;
-      clicOperateur = false;
-    }
-  }
+clavier.add(t1); clavier.add(t2); clavier.add(t3); 
+clavier.add(t4); clavier.add(t5); clavier.add(t6); 
+clavier.add(t7); clavier.add(t8); clavier.add(t9); 
+clavier.add(t10); clavier.add(t11); clavier.add(t12); 
 
+clavscien.add(carre); 
+clavscien.add(cube); 
+operation.add(plus); 
+operation.add(moins); 
+operation.add(divise); 
+operation.add(fois); 
+affichage= new JTextArea(); 
+affichage.setLayout(new BorderLayout()); 
+egal.add(tegal); 
+back.add(affichage,BorderLayout.NORTH); 
+back.add(operation,BorderLayout.WEST); 
+back.add(clavier,BorderLayout.CENTER); 
+back.add(clavscien,BorderLayout.EAST); 
+back.add(egal,BorderLayout.SOUTH); 
+all.setContentPane(back); 
+all.setVisible(true); 
+all.pack(); 
+} 
 
-  class Addition implements ActionListener {
-    public void actionPerformed(ActionEvent arg0){
-      if(clicOperateur){
-        calcul();
-        ecran.setText(String.valueOf(chiffre1));
-      }
-      else{
-        chiffre1 = Double.valueOf(ecran.getText()).doubleValue();
-        clicOperateur = true;
-      }
-      operateur = "+";
-      update = true;
-    }
-  }
+public void actionPerformed(ActionEvent evt) 
+{ 
+String a = evt.getActionCommand(); 
+affichage.setText(a); 
+} 
 
-
-  class Soustraction implements ActionListener {
-    public void actionPerformed(ActionEvent arg0){
-      if(clicOperateur){
-        calcul();
-        ecran.setText(String.valueOf(chiffre1));
-      }
-     else{
-        chiffre1 = Double.valueOf(ecran.getText()).doubleValue();
-        clicOperateur = true;
-      }
-      operateur = "-";
-      update = true;
-    }
-  }
-
-  class Multiplication implements ActionListener {
-    public void actionPerformed(ActionEvent arg0){
-      if(clicOperateur){
-        calcul();
-        ecran.setText(String.valueOf(chiffre1));
-      }
-      else{
-        chiffre1 = Double.valueOf(ecran.getText()).doubleValue();
-        clicOperateur = true;
-      }
-      operateur = "*";
-      update = true;
-    }
-  }
-
-
-  class Division implements ActionListener {
-    public void actionPerformed(ActionEvent arg0){
-      if(clicOperateur){
-        calcul();
-        ecran.setText(String.valueOf(chiffre1));
-      }
-      else{
-        chiffre1 = Double.valueOf(ecran.getText()).doubleValue();
-        clicOperateur = true;
-      }
-      operateur = "/";
-      update = true;
-    }
-  }
-
-  class Annulation implements ActionListener {
-    public void actionPerformed(ActionEvent arg0){
-      clicOperateur = false;
-      update = true;
-      chiffre1 = 0;
-      operateur = "";
-      ecran.setText("");
-    }
-  }
-}
+} 
